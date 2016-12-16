a584b63ad445d6649981a2f45acd80e2f0b0dd61
marty-hope <marty.hope@gmail.com>
2016-12-15 19:07:33 -0800

    Add some initial tests

---
+++ src/index.test.js
@@ -0,0 +1,25 @@
+import {expect} from 'chai';
+import jsdom from 'jsdom';
+import fs from 'fs';
+
+describe('Our first test', () => {
+  it('should pass', () => {
+    expect(true).to.equal(true);
+  });
+});
+
+describe('index.html', () => {
+  //include done when doing an asynchronous test
+  it('should say hello', (done) => {
+    const index = fs.readFileSync('./src/index.html', 'UTF-8');
+    //jsdom uses callback so requires
+    jsdom.env(index, (err, window) => {
+      const h1 = window.document.getElementsByTagName('h1')[0];
+      expect(h1.innerHTML).to.equal("Hello World!");
+      done();
+      window.close();
+    });
+  });
+});
+
+
