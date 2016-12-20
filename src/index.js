8f9d826c6e7a76ea9083e7496d05a42f657d1df5
marty-hope <marty.hope@gmail.com>
2016-12-19 16:53:14 -0800

    Add Delete functionality

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import './index.css'
-import {getUsers} from './api/userApi';
+import {getUsers, deleteUser} from './api/userApi';
 
 getUsers().then(result => {
 
@@ -8,7 +8,7 @@ getUsers().then(result => {
   result.forEach(user => {
 
     usersBody += `<tr>
-      <td>&nbsp;</td>
+      <td><a href="#" data-id="${user.id}" class="deleteUser">Delete</a></td>
       <td>${user.id}</td>
       <td>${user.firstName}</td>
       <td>${user.lastName}</td>
@@ -17,6 +17,18 @@ getUsers().then(result => {
   });
 
   global.document.getElementById('users').innerHTML = usersBody;
+
+  const deleteLinks = global.document.getElementsByClassName('deleteUser');
+
+  Array.from(deleteLinks, link => {
+    link.onclick = function(event){
+      const element= event.target;
+      event.preventDefault();
+      deleteUser(element.attributes["data-id"].value);
+      const row = element.parentNode.parentNode;
+      row.parentNode.removeChild(row);
+    };
+  });
 
 });
 
